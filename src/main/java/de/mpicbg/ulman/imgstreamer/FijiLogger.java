b3ce5163b5fd5cb7f9bee9c804e09ad5085dc4de
Vladimír Ulman <ulman@mpi-cbg.de>
2019-01-25 21:57:21 +0100

    ADD: SendImage+ReceiveImage sample Fiji plugins are testing the streaming

---
+++ src/main/java/de/mpicbg/ulman/imgstreamer/FijiLogger.java
@@ -0,0 +1,31 @@
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
+package de.mpicbg.ulman.imgstreamer;
+
+import org.scijava.app.StatusService;
+import org.scijava.log.LogService;
+
+public class FijiLogger implements ProgressCallback
+{
+	//singleton accross all loggers of this kind
+	final static long initTime = System.currentTimeMillis();
+
+	FijiLogger(final LogService _log, final StatusService _bar)
+	{ log = _log; bar = _bar; }
+
+	final LogService log;
+	final StatusService bar;
+
+	@Override
+	public void info(String msg)
+	{ System.out.println((System.currentTimeMillis()-initTime)+" : "+msg); }
+
+	@Override
+	public void setProgress(float howFar)
+	{ bar.showProgress((int)(100 * howFar), 100); }
+}
---
+++ src/main/java/de/mpicbg/ulman/imgstreamer/SendImage.java
@@ -0,0 +1,124 @@
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
+package de.mpicbg.ulman.imgstreamer;
+
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.ItemVisibility;
+import org.scijava.app.StatusService;
+import org.scijava.log.LogService;
+
+import net.imagej.ImageJ;
+import net.imagej.ImgPlus;
+
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.io.BufferedOutputStream;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.io.IOException;
+
+
+@Plugin(type = Command.class, menuPath = "File>Export>Send Current Image")
+public class SendImage implements Command
+{
+	@Parameter
+	private LogService log;
+
+	@Parameter
+	private StatusService status;
+
+	@Parameter
+	private ImgPlus<?> imgP;
+
+	// ----------- sending ----------- 
+	@Parameter(visibility = ItemVisibility.MESSAGE, persist = false, required = false)
+	private String remoteURLmsg = "Option A: Ask your receiving partner to tell you his address.";
+
+	@Parameter(label = "address:port of the receiving party:",
+			description = "The address can be anything as example.net or IP address"
+			+" as 10.0.0.2 delimited with ':' followed by a port number higher than"
+			+" 1024 such as 54545. It is important not to use any spaces.",
+			columns=15)
+	private String remoteURL = "replace_me:54545";
+
+	@Parameter(visibility = ItemVisibility.MESSAGE, persist = false, required = false)
+	private String sepMsgA = "------------------------------------------------------";
+
+	// ----------- serving ----------- 
+	@Parameter(visibility = ItemVisibility.MESSAGE, persist = false, required = false, initializer="getHostURL")
+	private String hostURLmsg = "";
+
+	private String hostURL = "";
+	void getHostURL() throws UnknownHostException
+	{
+		hostURL = InetAddress.getLocalHost().getHostAddress();
+		hostURLmsg = "Option B: Tell your downloading partner this address: ";
+		hostURLmsg += hostURL + ":";
+		hostURLmsg += new Integer(portNo).toString();
+	}
+
+	@Parameter(label = "port to listen at:", callback="getHostURL", min="1025", max="65535",
+			description = "The port number should be higher than 1024 such as 54545.")
+	private int portNo = 54545;
+
+	@Parameter(visibility = ItemVisibility.MESSAGE, persist = false, required = false)
+	private String firewallMsg = "Make sure the firewall is not blocking incoming connections to Fiji.";
+
+	@Parameter(visibility = ItemVisibility.MESSAGE, persist = false, required = false)
+	private String sepMsgB = "------------------------------------------------------";
+
+	// ----------- common ----------- 
+	@Parameter(label = "Choose the same sending option as your partner:", choices = {"A", "B"})
+	private char transferMode = 'A';
+
+	@Parameter(label = "Connection timeout in seconds:",
+			description = "The maximum time in seconds during which Fiji waits"
+			+" for establishing connection. If connection is not made after this period of time,"
+			+" no further attempts are made until this command is started again.",
+			min="1")
+	private int timeoutTime = 60;
+
+	// ----------- executive part ----------- 
+	@SuppressWarnings({"unchecked","rawtypes"})
+	@Override
+	public void run()
+	{
+		final FijiLogger flog = new FijiLogger(log, status);
+		try {
+			if (transferMode == 'A')
+			{
+				ImgStreamer isv = new ImgStreamer( flog );
+
+				isv.setImageForStreaming((ImgPlus)imgP);
+				System.out.println("stream length will be: "+isv.getOutputStreamLength());
+
+				final OutputStream os
+					= new BufferedOutputStream(new FileOutputStream("/tmp/out.dat"), 1024000);
+				isv.write(os);
+				os.close();
+			}
+			else
+			{
+				log.info("SendImage plugin: serving "+imgP.getName());
+			}
+		}
+		catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
+
+	public
+	static void main(final String... args)
+	{
+		final ImageJ ij = new net.imagej.ImageJ();
+		ij.ui().showUI();
+		//ij.command().run(machineGTViaMarkers.class, true);
+	}
+}
