21c47b9eadb19b11cbf9c0b3040f0a29bb8b121d
Matthias Arzt <arzt@mpi-cbg.de>
2019-04-26 16:44:39 +0200

    Add comparison to simple straigh forward serializing

--- src/test/java/de/mpicbg/ulman/imgstreamer/ImageStreamBenchmark.java
+++ src/test/java/de/mpicbg/ulman/imgstreamer/ImageStreamBenchmark.java
@@ -1,12 +1,14 @@
 package de.mpicbg.ulman.imgstreamer;
 
 import net.imagej.ImgPlus;
+import net.imglib2.Cursor;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.cell.CellImgFactory;
 import net.imglib2.type.numeric.integer.ShortType;
 import net.imglib2.util.Intervals;
+import net.imglib2.view.Views;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
@@ -20,11 +22,13 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
 
-@Fork( 0 )
+@Fork( 1 )
 @Warmup( iterations = 20, time = 100, timeUnit = TimeUnit.MILLISECONDS )
 @Measurement( iterations = 10, time = 100, timeUnit = TimeUnit.MILLISECONDS )
 @State( Scope.Benchmark )
@@ -59,7 +63,7 @@ public class ImageStreamBenchmark
 	}
 
 	@Benchmark
-	public RandomAccessibleInterval<ShortType> simple() throws IOException
+	public RandomAccessibleInterval<ShortType> pixelStreamer() throws IOException
 	{
 		// setup
 		ByteArrayOutputStream output = new ByteArrayOutputStream();
@@ -67,6 +71,25 @@ public class ImageStreamBenchmark
 		PixelStreamer.send( image, output );
 		InputStream input = new ByteArrayInputStream( output.toByteArray() );
 		return receive( input );
+	}
+
+	@Benchmark
+	public RandomAccessibleInterval<ShortType> simple() throws IOException
+	{
+		// setup
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		// run
+		DataOutputStream stream = new DataOutputStream( output );
+		final Cursor< ShortType > cursor = Views.flatIterable( image ).cursor();
+		while ( cursor.hasNext() )
+			stream.writeShort( cursor.next().getShort() );
+		final Img< ShortType > copy = ArrayImgs.shorts( Intervals.dimensionsAsLongArray( this.image ) );
+		InputStream input = new ByteArrayInputStream( output.toByteArray() );
+		DataInputStream stream1 = new DataInputStream( input );
+		Cursor< ShortType > cursor1 = Views.flatIterable( copy ).cursor();
+		while ( cursor1.hasNext() )
+			cursor1.next().set( stream1.readShort() );
+		return copy;
 	}
 
 	private RandomAccessibleInterval<ShortType> receive( InputStream input ) throws IOException
